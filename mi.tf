bac306ed100b0b86fd1cda7e6be55b423187ad8d
chrisorisawayi <christopher.orisawayi@hybridaccess.net>
2022-04-12 23:47:01 +0100

    updated mi resource group

--- mi.tf
+++ mi.tf
@@ -1,5 +1,5 @@
 resource "azurerm_user_assigned_identity" "mi" {
-  location            = azurerm_resource_group.node.location
+  location            = azurerm_resource_group.cluster.location
   name                = "aks-mi"
-  resource_group_name = azurerm_resource_group.node.name
+  resource_group_name = azurerm_resource_group.cluster.name
 }
