18d49cf2dd48ea877ec7499407fc8c6aaa1a4834
chrisorisawayi <christopher.orisawayi@hybridaccess.net>
2022-04-13 08:11:58 +0100

    updated output

--- main.tf
+++ main.tf
@@ -12,9 +12,9 @@ module "aks-bootstrap" {
 
 locals {
   kube-config = {
-    kube_host                   = module.aks.host.value
-    kube_client_certificate     = module.aks.client_certificate.value
-    kube_client_key             = module.aks.client_key.value
-    kube_cluster_ca_certificate = module.aks.cluster_ca_certificate.value
+    kube_host                   = module.aks.host
+    kube_client_certificate     = module.aks.client_certificate
+    kube_client_key             = module.aks.client_key
+    kube_cluster_ca_certificate = module.aks.cluster_ca_certificate
   }
 }
