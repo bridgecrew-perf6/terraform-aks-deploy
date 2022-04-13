6422d61c5171c6edb27f5b3606156f31b6659220
chrisorisawayi <christopher.orisawayi@hybridaccess.net>
2022-04-13 08:33:26 +0100

    updated module output

--- modules/aks-deploy/output.tf
+++ modules/aks-deploy/output.tf
@@ -1,20 +1,19 @@
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.aks.kube_config_raw
+  value = azurerm_kubernetes_cluster.aks.kube_admin_config_raw
 }
 
 output "client_key" {
-  value = azurerm_kubernetes_cluster.aks.kube_config.0.client_key
-  sensitive = true
+  value = azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_key
 }
 
 output "client_certificate" {
-  value = azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate
+  value = azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_certificate
 }
 
 output "cluster_ca_certificate" {
-  value = azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate
+  value = azurerm_kubernetes_cluster.aks.kube_admin_config.0.cluster_ca_certificate
 }
 
 output "host" {
-  value = azurerm_kubernetes_cluster.aks.kube_config.0.host
+  value = azurerm_kubernetes_cluster.aks.kube_admin_config.0.host
 }
