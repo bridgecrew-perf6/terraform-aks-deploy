9e5ae70920298a205feb8846c0f4c28dedabf8bc
chrisorisawayi <christopher.orisawayi@hybridaccess.net>
2022-04-13 08:09:33 +0100

    updated output

--- main.tf
+++ main.tf
@@ -2,5 +2,19 @@ module "aks" {
   source = "./modules/aks-deploy"
 
   environment = var.environment
+}
 
+/*
+module "aks-bootstrap" {
+  source = "./modules/aks-bootstrap"
+}
+*/
+
+locals {
+  kube-config = {
+    kube_host                   = module.aks.host.value
+    kube_client_certificate     = module.aks.client_certificate.value
+    kube_client_key             = module.aks.client_key.value
+    kube_cluster_ca_certificate = module.aks.cluster_ca_certificate.value
+  }
 }
--- modules/aks-deploy/output.tf
+++ modules/aks-deploy/output.tf
@@ -1,3 +1,20 @@
 output "kube_config" {
   value = azurerm_kubernetes_cluster.aks.kube_config_raw
 }
+
+output "client_key" {
+  value = azurerm_kubernetes_cluster.aks.kube_config.0.client_key
+  sensitive = true
+}
+
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate
+}
+
+output "cluster_ca_certificate" {
+  value = azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate
+}
+
+output "host" {
+  value = azurerm_kubernetes_cluster.aks.kube_config.0.host
+}
